b199a963c032ae5ffd04ee944965c6adf7072c7b
Rachele Suy <rachelesuy@gmail.com>
2019-08-02 14:09:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Rach"
+  end
+  
+  get '/hometown' do
+    "My hometown is P-A-P"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Jesus love u"
+    
+  end
+  
 end
